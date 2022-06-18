1f6a0137ee2cf5f5813f2cafa1a378b1f758af85
MatinT-SA <matin.taherzadeh.mmtsa@gmail.com>
2022-06-18 15:20:23 +0430

    The first commit of our Snake Game to our Repo

---
+++ game.js
@@ -0,0 +1,49 @@
+import { SNAKE_SPEED, update as updateSnake, draw as drawSnake, getSnakeHead, snakeIntersection } from './snake.js'
+import { update as updateFood, draw as drawFood } from './food.js'
+import { outsideGrid } from './grid.js'
+
+let lastRenderTime = 0
+let gameOver = false
+const gameBoard = document.getElementById('main-board')
+
+function main(currentTime) {
+    if (gameOver) {
+        // const gameOver = document.querySelector('#gameover-text')
+        // const playAgain = document.querySelector('#playAgain')
+        // gameOver.style.display = "block"
+        // playAgain.style.display = "block"
+
+        if (confirm('GAME OVER! 🥶\nTry again by pressing OK.')) {
+            window.location = '/'
+        }
+        return
+    }
+
+
+    window.requestAnimationFrame(main)
+    const secondsSinceLastRender = (currentTime - lastRenderTime) / 1000
+    if (secondsSinceLastRender < 1 / SNAKE_SPEED) return
+
+    lastRenderTime = currentTime
+
+    update()
+    draw()
+}
+
+window.requestAnimationFrame(main)
+
+function update() {
+    updateSnake()
+    updateFood()
+    checkDeath()
+}
+
+function draw() {
+    gameBoard.innerHTML = ''
+    drawSnake(gameBoard)
+    drawFood(gameBoard)
+}
+
+function checkDeath() {
+    gameOver = outsideGrid(getSnakeHead()) || snakeIntersection()
+}
